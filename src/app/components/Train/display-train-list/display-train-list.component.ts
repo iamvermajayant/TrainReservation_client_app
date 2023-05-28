4fefe41f05732b6739c39b5acfc5d0b2f77e4847
Panindhra <phanitallapudi@gmail.com>
2023-05-29 01:58:14 +0530

    added sorting for user train list.

--- src/app/components/Train/display-train-list/display-train-list.component.ts
+++ src/app/components/Train/display-train-list/display-train-list.component.ts
@@ -54,4 +54,89 @@ export class DisplayTrainListComponent implements OnInit {
       })
     }
   }
+  
+  // sortData(column: string): void {
+  //   switch (column) {
+  //     case 'TrainName':
+  //       this.trainsDisplayForUser.sort((a, b) => a.TrainName.localeCompare(b.TrainName));
+  //       break;
+  //     case 'TrainId':
+  //       this.trainsDisplayForUser.sort((a, b) => a.TrainId - b.TrainId);
+  //       break;
+  //     case 'Origin':
+  //       this.trainsDisplayForUser.sort((a, b) => a.Origin.localeCompare(b.Origin));
+  //       break;
+  //     case 'Destination':
+  //       this.trainsDisplayForUser.sort((a, b) => a.Destination.localeCompare(b.Destination));
+  //       break;
+  //     case 'Departure':
+  //       this.trainsDisplayForUser.sort((a, b) => new Date(a.Departure).getTime() - new Date(b.Departure).getTime());
+  //       break;
+  //     case 'Arrival':
+  //       this.trainsDisplayForUser.sort((a, b) => new Date(a.Arrival).getTime() - new Date(b.Arrival).getTime());
+  //       break;
+  //     case 'SeatCapacity':
+  //       this.trainsDisplayForUser.sort((a, b) => a.SeatCapacity - b.SeatCapacity);
+  //       break;
+  //     case 'SeatRate':
+  //       this.trainsDisplayForUser.sort((a, b) => a.SeatRate - b.SeatRate);
+  //       break;
+  //     default:
+  //       // No sorting logic for the selected column
+  //       break;
+  //   }
+  // }
+
+  sortBy: string | undefined;
+  sortDirection: string | undefined;
+
+  // Sort the data based on the selected column
+  sortData(column: string): void {
+    if (this.sortBy === column) {
+      // Reverse the sort direction if the same column is clicked again
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      // Set the new column and default sort direction
+      this.sortBy = column;
+      this.sortDirection = 'asc';
+    }
+
+    // Perform the sorting of the data
+    this.trainsDisplayForUser.sort((a, b) => {
+      const aValue = this.getPropertyValue(a, column);
+      const bValue = this.getPropertyValue(b, column);
+
+      if (aValue < bValue) {
+        return this.sortDirection === 'asc' ? -1 : 1;
+      } else if (aValue > bValue) {
+        return this.sortDirection === 'asc' ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  // Get the property value from the object based on the given column
+  getPropertyValue(obj: any, column: string): any {
+    const properties = column.split('.');
+    let value = obj;
+
+    for (const prop of properties) {
+      value = value[prop];
+    }
+
+    return value;
+  }
+
+
+  formatBookingDate(dateString: Date): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+  }
 }
